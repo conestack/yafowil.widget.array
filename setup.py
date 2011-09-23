f3e4655d112320ffc7b4ed677e8d9e4a731fbf8c
Robert Niederreiter <rnix@squarewave.at>
2011-09-23 12:32:12 +0200

    stub

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9dev'
+shortdesc = 'Array Widget for YAFOWIL - Yet Another Form Widget Library (Python, Web)'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+tests_require = ['yafowil[test]', 'yafowil.webob', 'gunicorn']
+
+setup(name='yafowil.widget.array',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Environment :: Web Environment',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'License :: OSI Approved :: BSD License',                    
+      ],
+      keywords='',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      url=u'https://github.com/bluedynamics/yafowil.widget.array',
+      license='Simplified BSD',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['yafowil', 'yafowil.widget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'yafowil',
+      ],
+      tests_require=tests_require,
+      extras_require = dict(
+          test=tests_require,
+      ),
+      test_suite="yafowil.widget.dict.tests.test_suite",
+      entry_points="""
+      # plone specific, ignore if not available
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,      
+      )
