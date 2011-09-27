d82c4f5ea5de8954e0453a64c5cb98ec0a9cc50b
Robert Niederreiter <rnix@squarewave.at>
2011-09-27 12:58:40 +0200

    more cleanup

--- src/yafowil/widget/array/widget.py
+++ src/yafowil/widget/array/widget.py
@@ -159,73 +159,6 @@ def duplicate_recursiv(widget):
         node[k] = duplicate_widget(v)
     return node
 
-#    static = widget.attrs['static']
-#    table = widget['table']
-#    table.attrs['id'] = 'dictwidget_%s.entry' % widget.dottedpath
-#    body = table['body']
-#    body.clear()
-#    if data.errors and static:
-#        basename = '%s.entry' % body.dottedpath
-#        value = extract_static(data, basename)
-#    else:
-#        value = fetch_value(widget, data)
-#    if not value:
-#        return
-#    i = 0
-#    for key, val in value.items():
-#        row = body['entry%i' % i] = factory('tr')
-#        k_props = {'class': 'key'}
-#        if static:
-#            k_props['disabled'] = 'disabled'
-#        row['key'] = factory(
-#            'td:text',
-#            value = key,
-#            name = 'key',
-#            props = k_props,
-#        )
-#        row['value'] = factory(
-#            'td:text',
-#            value = val,
-#            name = 'value',
-#            props = {
-#                'class': 'value',
-#            },
-#        )
-#        if not static:
-#            row['actions'] = factory(
-#                'td:dict_actions',
-#                props = {
-#                    'add': True,
-#                    'remove': True,
-#                    'up': True,
-#                    'down': True,
-#                },
-#            )
-#        i += 1
-
-
-#def raise_extraction_error(widget):
-#    if isinstance(widget.attrs['required'], basestring):
-#        raise ExtractionError(widget.attrs['required'])
-#    raise ExtractionError(widget.attrs['required_message'])
-
-
-#def extract_static(data, basename):
-#    request = data.request
-#    ret = odict()
-#    index = 0
-#    keys = data.value.keys()
-#    while True:
-#        valuename = '%s%i.value' % (basename, index)
-#        if request.has_key(valuename):
-#            if index >= len(keys):
-#                raise ExtractionError('invalid number of static values')
-#            ret[keys[index]] = request[valuename]
-#            index += 1
-#            continue
-#        break
-#    return ret
-
 
 #def extract_dynamic(data, basename):
 #    request = data.request
@@ -244,43 +177,8 @@ def duplicate_recursiv(widget):
 #    return ret
 
 
-#def dict_extractor(widget, data):
-#    static = widget.attrs['static']
-#    body = widget['table']['body']
-#    basename = '%s.entry' % body.dottedpath
-#    req = data.request
-#    index = 0
-#    if static:
-#        ret = extract_static(data, basename)
-#    else:
-#        ret = extract_dynamic(data, basename)
-#    if len(ret) == 0:
-#        ret = UNSET
-#    if widget.attrs.get('required'):
-#        if ret is UNSET:
-#            raise_extraction_error(widget)
-#        if static:
-#            for val in ret.values():
-#                if not val:
-#                    raise_extraction_error(widget)
-#    return ret
-
-
 def array_display_renderer(widget, data):
     return '<div>Array display</div>'
-
-#    value = data.value
-#    if not value:
-#        value = dict()
-#    values = list()
-#    for key, val in value.items():
-#        values.append(data.tag('dt', key) + data.tag('dd', val))
-#    head = u''
-#    if widget.attrs.get('head'):
-#        head = '%s: %s' % (widget.attrs['head']['key'],
-#                           widget.attrs['head']['value'])
-#        head = data.tag('h5', head)
-#    return head + data.tag('dl', *values)
 
 
 factory.register(
