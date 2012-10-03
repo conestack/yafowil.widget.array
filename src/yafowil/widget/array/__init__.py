ed8fe141fa15aecbc05168203903b383516c3bcf
Robert Niederreiter <office@squarewave.at>
2012-10-03 10:48:00 +0200

    resource group names

--- src/yafowil/widget/array/__init__.py
+++ src/yafowil/widget/array/__init__.py
@@ -4,17 +4,17 @@ from yafowil.base import factory
 
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 js = [{
-    'group': 'yafowil.widget.array',
+    'group': 'yafowil.widget.array.common',
     'resource': 'widget.js',
     'order': 20,
 }]
 default_css = [{
-    'group': 'yafowil.widget.array',
+    'group': 'yafowil.widget.array.common',
     'resource': 'default/widget.css',
     'order': 20,
 }]
 bootstrap_css = [{
-    'group': 'yafowil.widget.array',
+    'group': 'yafowil.widget.array.common',
     'resource': 'bootstrap/widget.css',
     'order': 20,
 }]
