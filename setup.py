10dc6f6cd0fca67781d98263f20c94643ee39d84
Robert Niederreiter <office@squarewave.at>
2012-11-02 19:44:51 +0100

    "formatting"

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
 
 version = '1.2.1'
 shortdesc = 'Array Widget for YAFOWIL'
@@ -8,16 +12,17 @@ longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 tests_require = ['yafowil[test]']
 
+
 setup(name='yafowil.widget.array',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Environment :: Web Environment',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python', 
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'License :: OSI Approved :: BSD License',                    
+          'Environment :: Web Environment',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python', 
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'License :: OSI Approved :: BSD License',                    
       ],
       keywords='',
       author='BlueDynamics Alliance',
@@ -43,5 +48,4 @@ setup(name='yafowil.widget.array',
               'register = yafowil.widget.array:register',
               'example = yafowil.widget.array.example:get_example'
           ],
-      },
-      )
+      })
