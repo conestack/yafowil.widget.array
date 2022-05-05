a578739c9653d398c57b1434c37b466df0c004c1
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-05-05 12:21:24 +0200

    rudimentary test setup

--- js/tests/test_array.js
+++ js/tests/test_array.js
@@ -1,3 +1,10 @@
-QUnit.test('test', assert => {
-    assert.ok(true);
+import { ArrayWidget } from "../src/widget";
+
+QUnit.test('Initialize', assert => {
+    let el = $('<div class="array" id="test" />').appendTo('body');
+    ArrayWidget.initialize();
+
+    let wid = el.data('array');
+    assert.ok(wid);
+    assert.true(wid instanceof ArrayWidget);
 });
