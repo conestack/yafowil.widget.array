a837b0bb66bc6d82df498c76b0cdbf1423bb0966
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-05-06 10:27:00 +0200

    remove css sourcemaps

--- scripts/styles.sh
+++ scripts/styles.sh
@@ -4,6 +4,6 @@ SASS_BIN="./node_modules/sass/sass.js"
 SASS_DIR="./scss"
 TARGET_DIR="./src/yafowil/widget/array/resources"
 
-$SASS_BIN $SASS_DIR/widget_default.scss $TARGET_DIR/default/widget.css
-$SASS_BIN $SASS_DIR/widget_bootstrap.scss $TARGET_DIR/bootstrap/widget.css
-$SASS_BIN $SASS_DIR/widget_plone5.scss $TARGET_DIR/plone5/widget.css
+$SASS_BIN $SASS_DIR/widget_default.scss --no-source-map $TARGET_DIR/default/widget.css
+$SASS_BIN $SASS_DIR/widget_bootstrap.scss --no-source-map $TARGET_DIR/bootstrap/widget.css
+$SASS_BIN $SASS_DIR/widget_plone5.scss --no-source-map $TARGET_DIR/plone5/widget.css
