62a01e8c6ba0ee4824446e3d814d35f952f2a279
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 13:11:35 +0200

    "Restore bdajax compat"

--- js/src/bundle.js
+++ js/src/bundle.js
@@ -7,6 +7,8 @@ export * from './widget.js';
 $(function() {
     if (window.ts !== undefined) {
         ts.ajax.register(ArrayWidget.initialize, true);
+    } else if (window.bdajax !== undefined) {
+        bdajax.register(ArrayWidget.initialize, true);
     } else {
         ArrayWidget.initialize();
     }
