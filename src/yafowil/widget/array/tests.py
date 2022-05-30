6161c6c51bc6632e002d34b4c12cb1ada7f6f666
Robert Niederreiter <office@squarewave.at>
2022-05-30 10:57:20 +0200

    "Add resource tests"

--- src/yafowil/widget/array/tests.py
+++ src/yafowil/widget/array/tests.py
@@ -5,20 +5,25 @@ from yafowil.base import factory
 from yafowil.compat import IS_PY2
 from yafowil.tests import fxml
 from yafowil.tests import YafowilTestCase
+import os
 import unittest
-import yafowil.loader  # noqa
 
 
 if not IS_PY2:
     from importlib import reload
 
 
+def np(path):
+    return path.replace('/', os.path.sep)
+
+
 class TestArrayWidget(YafowilTestCase):
 
     def setUp(self):
         super(TestArrayWidget, self).setUp()
-        from yafowil.widget.array import widget
-        reload(widget)
+        from yafowil.widget import array
+        reload(array.widget)
+        array.register()
 
     def test_array_with_missing_entry_definition(self):
         form = factory(
@@ -2123,6 +2128,76 @@ class TestArrayWidget(YafowilTestCase):
           </div>
         </form>
         """, fxml(form()))
+
+    def test_resources(self):
+        factory.theme = 'default'
+        resources = factory.get_resources('yafowil.widget.array')
+        self.assertTrue(resources.directory.endswith(np('/array/resources')))
+        self.assertEqual(resources.path, 'yafowil-array')
+
+        scripts = resources.scripts
+        self.assertEqual(len(scripts), 1)
+
+        self.assertTrue(scripts[0].directory.endswith(np('/array/resources')))
+        self.assertEqual(scripts[0].path, 'yafowil-array')
+        self.assertEqual(scripts[0].file_name, 'widget.min.js')
+        self.assertTrue(os.path.exists(scripts[0].file_path))
+
+        styles = resources.styles
+        self.assertEqual(len(styles), 1)
+
+        self.assertTrue(
+            styles[0].directory.endswith(np('/array/resources/default'))
+        )
+        self.assertEqual(styles[0].path, 'yafowil-array/default')
+        self.assertEqual(styles[0].file_name, 'widget.css')
+        self.assertTrue(os.path.exists(styles[0].file_path))
+
+        factory.theme = 'bootstrap3'
+        resources = factory.get_resources('yafowil.widget.array')
+        self.assertTrue(resources.directory.endswith(np('/array/resources')))
+        self.assertEqual(resources.path, 'yafowil-array')
+
+        scripts = resources.scripts
+        self.assertEqual(len(scripts), 1)
+
+        self.assertTrue(scripts[0].directory.endswith(np('/array/resources')))
+        self.assertEqual(scripts[0].path, 'yafowil-array')
+        self.assertEqual(scripts[0].file_name, 'widget.min.js')
+        self.assertTrue(os.path.exists(scripts[0].file_path))
+
+        styles = resources.styles
+        self.assertEqual(len(styles), 1)
+
+        self.assertTrue(
+            styles[0].directory.endswith(np('/array/resources/bootstrap'))
+        )
+        self.assertEqual(styles[0].path, 'yafowil-array/bootstrap')
+        self.assertEqual(styles[0].file_name, 'widget.css')
+        self.assertTrue(os.path.exists(styles[0].file_path))
+
+        factory.theme = 'plone5'
+        resources = factory.get_resources('yafowil.widget.array')
+        self.assertTrue(resources.directory.endswith(np('/array/resources')))
+        self.assertEqual(resources.path, 'yafowil-array')
+
+        scripts = resources.scripts
+        self.assertEqual(len(scripts), 1)
+
+        self.assertTrue(scripts[0].directory.endswith(np('/array/resources')))
+        self.assertEqual(scripts[0].path, 'yafowil-array')
+        self.assertEqual(scripts[0].file_name, 'widget.min.js')
+        self.assertTrue(os.path.exists(scripts[0].file_path))
+
+        styles = resources.styles
+        self.assertEqual(len(styles), 1)
+
+        self.assertTrue(
+            styles[0].directory.endswith(np('/array/resources/plone5'))
+        )
+        self.assertEqual(styles[0].path, 'yafowil-array/plone5')
+        self.assertEqual(styles[0].file_name, 'widget.css')
+        self.assertTrue(os.path.exists(styles[0].file_path))
 
 
 if __name__ == '__main__':
