e09a4865da87c992749fb81b3661f8fe722772cf
Robert Niederreiter <office@squarewave.at>
2022-10-06 09:52:22 +0200

    Back to development: 1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.7'
+version = '1.8.dev0'
 shortdesc = 'Array Widget for YAFOWIL'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
